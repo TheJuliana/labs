5d2ce97229670eed8c15d8723d45db243b40b248
Ульяна Светлакова <62110361+TheJuliana@users.noreply.github.com>
2021-12-11 09:11:57 +0200

    Create main.cpp

---
+++ !ADDITIONALS/lab1_add/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+using namespace std;
+int main() {
+    // Q1 т. - количество тонн груза за одну поездку первого типа
+    //P1 - цена поездки первого типа
+    //Q2 - количество тонн груза за одну поездку второго типа
+    //P2 - цена поездки второго типа
+    //A - количество тонн груза всего
+    int Q1, P1, Q2, P2, A;
+    cin >> Q1 >> P1 >> Q2 >> P2 >> A;
+
+    //проверка условия
+    if ((Q1 >= 1000) || (P1 >= 1000) || (Q2 >= 1000) || (P2 >= 1000) || (A >= 1000)) {
+        cout << "Invalid input data format" << endl;
+        return 0;
+    }
+
+    //проверка времени программы
+    auto begin = std::chrono::steady_clock::now();
+
+    int val1 = P1/Q1; //цена за одну тонну
+    int val2 = P2/Q2;
+    int sum = 0;
+    int n;
+    float rem;
+    if (val1 < val2) {
+        n = A/Q1;
+        rem = A%Q1;
+        sum = n*P1;
+        if ((rem < Q2) && (P2 < P1)) {
+            sum += P2;
+        } else if ((rem > Q2) && (P2 < P1)) {
+            sum += ceil(rem/Q2)*P2;
+        } else if ((rem < Q1) && (P1 < P2)) {
+            sum += P1;
+        } else {
+            sum += ceil(rem/Q1)*P1;
+        }
+    } else {
+        n = A/Q2;
+        rem = A%Q2;
+        sum = n*P2;
+        if ((rem < Q1) && (P1 < P2)) {
+            sum += P1;
+        } else if ((rem > Q1) && (P1 < P2)) {
+            sum += ceil(rem/Q1)*P1;
+        } else if ((rem < Q2) && (P2 < P1)) {
+            sum += P2;
+        } else {
+            sum += ceil(rem/Q2)*P2;
+        }
+    }
+    cout << sum << endl;
+
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+    std::cout << "Runtime: " << elapsed_ms.count() << " ms\n";
+
+    return 0;
+}
