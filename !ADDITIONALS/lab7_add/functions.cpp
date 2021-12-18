3cd5f77234327247b9df2cbddfaeb3a45be5b60e
Ульяна Светлакова <62110361+TheJuliana@users.noreply.github.com>
2021-12-18 16:28:14 +0200

    Add files via upload

---
+++ !ADDITIONALS/lab7_add/functions.cpp
@@ -0,0 +1,47 @@
+#include "functions.hpp"
+
+namespace fn{
+    void Read(int &n, std::vector<int> &seq) {
+        std::cin >> n;
+        for (int i = 0; i < n; i++) {
+            int x;
+            std::cin >> x;
+            seq.push_back(x);
+        }
+    }
+     bool Nine(int x) {
+         int num = x;
+         std::vector<int> digits;
+         while (num > 0) {
+             digits.push_back(num % 10);
+             num /= 10;
+         }
+         for (auto& digit : digits) {
+             if (digit == 9) {
+                 return true;
+             }
+         }
+         return false;
+     }
+
+     bool Doubl(int x) {
+        int num = x;
+         bool arr[10]={false};
+         while(num != 0){
+             int d = num%10;
+             if( arr[d] ){
+                 return true;
+             }
+             arr[d] = true;
+             num /= 10;
+         }
+         return false;
+    }
+
+    void Output(std::vector<int> seq) {
+        for (auto& num : seq) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+}
---
+++ !ADDITIONALS/lab7_add/functions.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+namespace fn{
+    void Read(int &n, std::vector<int> &seq);
+    bool Nine(int x);
+    bool Doubl(int x);
+    void Output(std::vector<int> seq);
+}
