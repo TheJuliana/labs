1401672de0946d5fb10ee52ea86ec663eec6b790
Ульяна Светлакова <62110361+TheJuliana@users.noreply.github.com>
2022-01-20 10:38:54 +0200

    Update Functions.hpp

--- !ADDITIONALS/lab3_add/Functions.hpp
+++ !ADDITIONALS/lab3_add/Functions.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace fn {
-    void Read(int mas[1000]);
+    void Read(int mas[1000], int& x);
     void Replace(int mas[1000], int n);
     void Output(int mas[1000], int n);
 }
